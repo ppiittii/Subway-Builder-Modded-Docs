39c085a104d0146e44ced183954220fc6c5b24c6
kaicardenas0618 <kcardenasit@gmail.com>
2026-02-19 22:20:03 -0500

    Add homepage

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+
+export default function Home() {
+  return (
+    <Layout
+      title="Home"
+      description="Welcome to Subway Builder Modded Docs">
+      <main style={{ padding: '2rem', textAlign: 'center' }}>
+        <h1>Welcome to Subway Builder Modded Docs!</h1>
+        <p>
+          Use the sidebar to navigate maps, mods, and tutorials.
+        </p>
+      </main>
+    </Layout>
+  );
+}
